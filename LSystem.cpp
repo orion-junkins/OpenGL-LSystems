39e6d13121f89cb590b2a4c034fce7af4daa17e6
Orion Junkins <orionjunkins@gmail.com>
2021-12-03 16:35:26 -0800

    grammar parsing functionality moved into LSystem class

--- LSystem.cpp
+++ LSystem.cpp
@@ -1,12 +1,35 @@
 #include "./LSystem.h"
-//#include <map>
-//#include "Point.h"
-//typedef std::map<char, std::string> BasePairMap;
-
-LSystem::LSystem(string axiom, float rotation_angle){
+#include <iostream>
+LSystem::LSystem(string axiom, int drawing_depth, float rotation_angle, float depth_scale_factor){
     m_axiom = axiom;
+    m_instruction = axiom;
+    m_drawing_depth = drawing_depth;
     m_rotation_angle = rotation_angle;
+    m_depth_scale_factor = depth_scale_factor;
 }
+
 void LSystem::addRule(char input_character, string substitution){
     m_rules[input_character] = substitution;
 }
+
+string LSystem::substitute(char input) {
+  string result = "";
+  if ( m_rules.find(input) == m_rules.end() ) {
+    result += input;
+  } else {
+    result += m_rules[input];
+  }
+  return result;
+}
+
+void LSystem::generateInstruction() {
+    for(int i = m_drawing_depth; i > 0; i--){
+        string working_instruction = "";
+        for(char& curChar : m_instruction) {
+            working_instruction += substitute(curChar);
+        }
+        // cout << working_instruction << endl;
+        m_instruction = working_instruction;
+    }
+}
+
--- LSystem.h
+++ LSystem.h
@@ -8,14 +8,22 @@ typedef map<char, string> BasePairMap;
 
 class LSystem {
     private:
-        float m_rotation_angle;
+        float recursion_depth = 1.0;
     public:
         string m_axiom;
+        int m_drawing_depth;
+        float m_rotation_angle;
+        float m_depth_scale_factor;
         BasePairMap m_rules;
+        string m_instruction;
 
-        LSystem(string axiom, float rotation_angle);
-        
+        LSystem(string axiom, int drawing_depth, float rotation_angle, float depth_scale_factor);
+
         void addRule(char input_character, string substitution);
+
+        string substitute(char input);
+
+        void generateInstruction();
 };
 
 #endif //LSYSTEM_H
